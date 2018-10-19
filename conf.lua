64b24548a170daa395afd123acdfa87ab976920c
Stuwert <stuart@urback.net>
2018-10-18 23:14:15 -0600

    Working plane

--- conf.lua
+++ conf.lua
@@ -2,9 +2,10 @@
 
 function love.conf(t)
     t.title = "Scrolling Shooter Tutorial"
-    t.version = "0.9.1"
+    t.version = "11.1"
     t.window.width = 480
     t.window.height = 800
 
     -- For Windows debugging
     t.console = true
+end
--- main.lua
+++ main.lua
@@ -1,17 +1,30 @@
-function love.load()
+playerImg = nil -- for storage
+player = { x = 200, y = 710, speed = 150, img = nil}
 
+function love.load(arg)
+    player.img = love.graphics.newImage('aircrafts/Aircraft_03.png')
 end
 
 function love.update(dt)
-
-end
-
-function love.keypressed(key)
-    if key == "escape" then
-        love.event.push("quit)
+    if love.keyboard.isDown("escape") then
+        love.event.push("quit")
+    end
+    if love.keyboard.isDown('left', 'a') then
+        player.x = player.x - (player.speed*dt)
+    elseif love.keyboard.isDown('right', 'd') then 
+        player.x = player.x + (player.speed*dt)
+    end
+    if love.keyboard.isDown('up', 'w') then
+        player.y = player.y - (player.speed*dt)
+    elseif love.keyboard.isDown('down', 's') then 
+        player.y = player.y + (player.speed*dt)
     end
 end
 
-function love.draw(dt)
+-- function love.keypressed(key)
+    
+-- end
 
+function love.draw(dt)
+    love.graphics.draw(player.img, player.x, player.y)
 end
